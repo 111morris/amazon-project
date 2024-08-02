be7e7623a74ea8a3723367d855b6b669b7254827
Morris Mulandi <mulandimorris1@gmail.com>
2024-08-02 10:14:31 +0300

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
   });
   cartSummaryHTML +=
     `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
    <div class="delivery-date">
     Delivery date: Tuesday, June 21
    </div>
@@ -89,7 +89,12 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
 document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
-    const productId = link.dataset.productId;
-    removeFromCart(productId);
+    if (confirm('Do you want to delete this product')) {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+  
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.remove()      
+    };
   });
 });
